b41368f49530b81f25504a69cc5bd691a4e00872
TetianaFesh <49169602+TetianaFesh@users.noreply.github.com>
2020-11-16 18:49:06 +0200

    Add controllers

---
+++ PF.Api/Controllers/PeopleController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PF.Api.Services;
+using PF.Data;
+using PF.Data.Models;
+
+namespace PF.Api.PeopleController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IPensionCalculator _calculator;
+
+        public PeopleController(ApplicationDbContext context, IPensionCalculator calculator)
+        {
+            _context = context;
+            _calculator = calculator;
+        }
+
+        // GET: api/People
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPeople()
+        {
+            return await _context.People.Include(p => p.Modifier).ToListAsync();
+        }
+
+        // GET: api/People/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPerson(Guid id)
+        {
+            var person = await _context.People.AsNoTracking()
+                .Include(p => p.Modifier)
+                .Include(p => p.Experiences)
+                .ThenInclude(e => e.Position)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+
+        [HttpGet("{id}/pension")]
+        public async Task<ActionResult<double>> GetPension(Guid id)
+        {
+            var person = await _context.People.AsNoTracking()
+                .Include(p => p.Modifier)
+                .Include(p => p.Experiences)
+                .ThenInclude(e => e.Position)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return _calculator.Calculate(person);
+        }
+
+        // PUT: api/People/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPerson(Guid id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+        // POST: api/People
+        [HttpPost]
+        public async Task<ActionResult<Person>> PostPerson(Person person)
+        {
+            _context.People.Add(person);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new { id = person.Id }, person);
+        }
+
+        // DELETE: api/People/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Person>> DeletePerson(Guid id)
+        {
+            var person = await _context.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _context.People.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return person;
+        }
+
+        private bool PersonExists(Guid id)
+        {
+            return _context.People.Any(e => e.Id == id);
+        }
+    }
+}
